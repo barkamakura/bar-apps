131a4d0d36ca39baf70da5c548ef36948fadef25
barkamakura <teru.clude@gmail.com>
2026-09-11 07:25:14 +0900

    Add files via upload

--- denpyo/sw.js
+++ denpyo/sw.js
@@ -2,13 +2,11 @@
 // アプリの画面ファイルだけを端末に保存します。
 // お店のデータ(jsonbin)は必ずネット経由で取りに行くので、古いデータが表示されることはありません。
 
-const CACHE = "bk-app-v24";
+const CACHE = "bk-denpyo-v24";
 const SHELL = [
   "./",
   "./index.html",
-  "./price.html",
   "./slip.html",
-  "./records.html",
   "./manifest.json",
   "./icon-32.png",
   "./icon-180.png",
